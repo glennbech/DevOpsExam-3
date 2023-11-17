dd5e370a1bfb11c0ec5aa4bb1ac7ba6e74f7b52b
Arnold <arnold.gonczlik@hotmail.com>
2023-11-17 05:02:49 +0100

    terraform gitignore, added vars to IoC, started on provider.tf

--- infra/vars.tf
+++ infra/vars.tf
@@ -24,12 +24,17 @@ variable "iam_policy_name" {
 
 variable "cpu_units" {
   description = "Number of CPU units reserved for each instance"
-  type = number
-  default = 256
+  type = string
+  default = "256"
 }
 
 variable "memory_amount" {
   description = "Amount of memory in MB reserved for each instance"
   type = number
   default = 1024
+}
+
+variable "AWS_REGION" {
+  type = string
+  default = "eu-west-1"
 }
