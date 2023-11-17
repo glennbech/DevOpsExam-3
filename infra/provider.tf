2e1b1fd376f20df4deaf7436881cfd6c76b7824c
Arnold <arnold.gonczlik@hotmail.com>
2023-11-17 05:12:06 +0100

    terraform gitignore, added vars to IoC, remote state, provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,17 @@
 provider "aws"{
   region = var.AWS_REGION
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.39.0"
+    }
+  }
+  backend "s3" {
+    bucket = "pgr301-2021-terraform-state"
+    key    = "kand2035/apprunner-actions.state"
+    region = var.AWS_REGION
+  }
+}
